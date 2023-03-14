e42548cde5aa6bd21d367070e2416141d18e2c75
GraciaSoriano <127821484+GraciaSoriano@users.noreply.github.com>
2023-03-14 16:44:37 -0600

    Add files via upload

---
+++ Tarea Polimorfismo/Ejercicio1.py
@@ -0,0 +1,14 @@
+class Hermano1 :
+    pass
+    def edad():
+        return 'La edad de Josue es de 20'
+    
+class Hermano2 :
+    pass
+    def edad():
+        return'La edad de Juan es de 17'
+    
+hermano1 = Hermano1
+hermano2 = Hermano2
+print(hermano1.edad())
+print(hermano2.edad())
---
+++ Tarea Polimorfismo/Ejercicio2.py
@@ -0,0 +1,14 @@
+class Mascota1 :
+    pass
+    def nombre():
+        return 'El nombre de mi periquito es zazu'
+    
+class Mascota2 :
+    pass
+    def nombre():
+        return'El nombre de conejita es negra'
+    
+mascota1 = Mascota1
+mascota2 = Mascota2
+print(mascota1.nombre())
+print(mascota2.nombre())
